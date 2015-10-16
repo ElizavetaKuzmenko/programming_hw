39d33faf7223d3512b4ed340a84a435a4cd780cd
ElizavetaKuzmenko <lizaku77@gmail.com>
2015-10-16 14:36:46 +0300

    classwork on 16.10 with unittests

---
+++ hw5.py
@@ -0,0 +1,42 @@
+__author__ = 'liza'
+
+import unittest, re
+
+class TestFibonacci(unittest.TestCase):
+    def setUp(self):  # операции, которые надо выполнить для любого теста
+        pass
+
+    def test_math(self):
+        self.assertEqual(fib(0), 1)
+        self.assertEqual(fib(1), 1)
+        self.assertEqual(fib(14), 8)
+
+    def test_bad_data(self):
+        self.assertEqual(fib('котик'), -1)
+
+class TestDrink(unittest.TestCase):
+    def test_re(self):
+        self.assertTrue(drink('выпью'))
+        self.assertFalse(drink('запивать'))
+        self.assertTrue(drink('выпитую'))
+        self.assertFalse(drink('пьяница'))
+
+def fib(n):
+    if type(n) is not int:
+        return -1
+    elif n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 2) + fib(n - 1)
+
+def drink(string):
+    if re.search('^вып(ью|ьем|ьешь|ьет|ьют|ит|ив|ил)', string.lower()) is None:
+        return False
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)  # to write about tests that are successfully executed
